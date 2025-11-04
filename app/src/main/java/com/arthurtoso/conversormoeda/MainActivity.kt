dea82cd165e617763c51c66642aecce3a5df79e0
Gabriel-Kaue <gabrielkauecaitan099@gmail.com>
2025-11-04 19:33:14 -0300

    fução on click vazia.

--- app/src/main/java/com/arthurtoso/conversormoeda/MainActivity.kt
+++ app/src/main/java/com/arthurtoso/conversormoeda/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.arthurtoso.conversormoeda
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    fun onConvertButtonClick(view: View) {}
 }
